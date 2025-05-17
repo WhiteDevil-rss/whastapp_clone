a50635888f6fbd2783c58b49a6778bb3168f3e33
Mr.WhiteDevil <40131496+WhiteDevil-rss@users.noreply.github.com>
2025-05-17 10:52:19 +0530

    Update index.js for update port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 const methodOverride = require('method-override');
